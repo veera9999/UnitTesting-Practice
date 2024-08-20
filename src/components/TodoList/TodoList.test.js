00789657702df3f6f4a37f9f902c9fe3fcfc073e
Veera Palla <veera.pa999@gmail.com>
2024-08-19 20:05:59 -0500

    INITIAL COMMIT

---
+++ src/components/TodoList/TodoList.test.js
@@ -0,0 +1,20 @@
+import TodoList from "./TodoList";
+
+// TODO: Mock the fetch API, and do reset and clean up
+beforeEach(() => {});
+
+afterEach(() => {});
+
+afterAll(() => {});
+
+// TODO: Test component to render correctly with the fetched data
+test("renders fetched todos on mount", async () => {});
+
+// TODO: Test component to handle API fetch failure and display error message
+test("handles API fetch failure", async () => {});
+
+// TODO: Test adding a new todo
+test("adds a new todo item", async () => {});
+
+// TODO: Test removing a todo
+test("removes a todo item", async () => {});
